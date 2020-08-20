53a2f9cc07dd6d91498d991814e7ba9492e32305
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2020-08-21 01:05:36 +0600

    LightOJ - 1297

--- LightOJ_Solutions/Math/1297 - Largest Box.cc
+++ LightOJ_Solutions/Math/1297 - Largest Box.cc
@@ -1,1 +1,68 @@
 
+
+// </> : towhid1zaman
+
+#pragma GCC optimize("O3")
+#pragma GCC target("sse4")
+// #pragma GCC optimize("Ofast")  
+// #pragma GCC target("avx,avx2,fma") 
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair< int, int > pii;
+typedef vector<pii>vip;
+typedef pair< pii, int > ppi;
+#define fill(a,b) memset(a,b,sizeof(a))
+#define all(v) (v).begin(),(v).end()
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define rep(i,a) for(int i=0;i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define irep(i,b,a) for(int i=(b);i>=(a);--i)
+#define minv(v) *min_element(all(v))
+#define maxv(v) *max_element(all(v))
+#define unq(v) sort(all(v)),(v).erase(unique(all(v)),(v).end())
+#define _ ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl "\n"
+#define sqr(a) ((a)*(a))
+#define sz(a) int(a.size())
+#define ff first
+#define ss second
+#define pb push_back
+
+const double pi = acos(-1.0);
+const int mod = 1000000007; // (int)1e9+7
+const int inf = 0x3f3f3f3f;// (int)3e18;
+const int maxn = 2000;
+ /*
+    after derivation finding root of quadratic equation of v = (L-2x)(W-2x)(x)
+    x = (L+w-sqrt(L^2 + W^2 - LW))/6
+ */
+
+int cs = 1;
+void task(){
+    double L,W,X,V;
+    cin >> L >> W;
+    X = (L+W-sqrt(L*L + W*W - L*W))/6;
+    V =  (L-2*X)*(W-2*X)*(X);
+    sp(10);
+    cout << "Case "<<cs++<<": "<<V<<endl;
+}
+
+int main(){
+        //_
+        #ifdef OJ
+        freopen("inputf.in", "r", stdin);
+        freopen("outputf.in", "w", stdout);
+        #endif
+
+        int T = 1; cin >> T;
+        while(T--){
+            task();
+        }
+        
+return 0;
+}
+
+
