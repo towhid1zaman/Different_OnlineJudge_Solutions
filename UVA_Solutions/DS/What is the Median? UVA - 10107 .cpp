825a10caa012da085a226110ed906611aada63b3
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2020-08-31 22:51:33 +0600

    vector, sorting

---
+++ UVA_Solutions/DS/What is the Median? UVA - 10107 .cpp
@@ -0,0 +1,91 @@
+
+// </> : towhid1zaman
+
+#pragma GCC optimize("O3")
+#pragma GCC target("sse4")
+#pragma GCC optimize("Ofast")  
+#pragma GCC target("avx,avx2,fma") 
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+
+#define endl "\n"
+#define sqr(a) ((a)*(a))
+#define sz(a) int(a.size())
+#define ff first
+#define ss second
+#define pb push_back
+#define eb emplace_back
+#define deb(x) cout<<#x <<"="<<x<<endl;
+#define present(v,x) ((v).find(x)!=(v).end())
+#define _ ios::sync_with_stdio(0);cin.tie(0);
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define fill(a,b) memset(a,b,sizeof(a))
+#define all(v) (v).begin(),(v).end()
+
+
+#define rep(i,a) for(int i=0;i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define irep(i,b,a) for(int i=(b);i>=(a);--i)
+
+#define minv(v) *min_element(all(v))
+#define maxv(v) *max_element(all(v))
+#define unq(v) sort(all(v)),(v).erase(unique(all(v)),(v).end())
+#define yn(k) if(k)puts("YES");else puts("NO");
+
+const double pi = acos(-1.0);
+const int mod = 1000000007; // (int)1e9+7
+const int inf = 0x3f3f3f3f;// (int)3e18;
+const int maxn = 200005;
+
+/*
+ *
+ */
+
+void task(){
+    std::vector<int> v;
+    int n;
+    while(cin >> n){
+    	v.pb(n);
+    	sort(all(v));
+    	if(v.size()==1){
+    		cout << v[0] << endl;
+    	}
+    	else{
+    		if(v.size()%2==1){
+    			int mid = (v.size()+1)/2;
+    			cout << v[mid-1] << endl;
+    		}
+    		else{
+    			int a = v.size()/2;
+    			a--;
+    			cout << (v[a]+v[a+1])/2 << endl;
+    		}
+    	}
+    }    
+}
+
+int main(){
+        _
+        #ifdef OJ
+        freopen("inputf.in", "r", stdin);
+        freopen("outputf.in", "w", stdout);
+        #endif
+
+        int T = 1; //cin >> T;
+        while(T--){
+            task();
+        }
+        
+        
+return 0;
+}
+
+
