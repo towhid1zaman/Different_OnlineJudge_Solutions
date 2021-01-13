61a18128596d99c0af8d148343be0ac35ba633cf
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2021-01-13 15:16:02 +0600

    LOJ-1136

--- LightOJ_Solutions/Warmup/Division by 3  LOJ-1136.cpp
+++ LightOJ_Solutions/Warmup/Division by 3  LOJ-1136.cpp
@@ -8,7 +8,7 @@ const int maxn = 200005;
 const int mod = 1000000007;
 
 /*
-every 3 consicutive numbers sum is divisible by 3
+every 3 consicutive numbers figits sum is divisible by 3
 means, 123, 345,... x,x+1,x+2 = 3x+3 = 3(x+3)
 so, 1 to n, has (n/3) number of tuple and every tuple has 2 number that is fivisible by 3 
 so, 1---n => (n/3)*2 number that divisible by 3
