8ce66ae1277cdc412126389e3af4a270e9c2b6eb
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2021-01-30 03:26:21 +0600

    10

---
+++ Project_Euler/Summation of primes.cpp
@@ -0,0 +1,47 @@
+#pragma comment (linker,"/STACK:16777216")
+#include "bits/stdc++.h"
+using namespace std;
+typedef  unsigned long long ll;
+#define all(v) (v).begin(),(v).end()
+#define endl "\n"
+const int maxn = 2000000;
+const int mod = 1000000007;
+/*
+Problem statement:Find the sum of all the primes below two million.
+
+*/
+
+vector<int> primes;
+bool isp[maxn]; 
+void sieve() {//true means prime
+    fill(isp+2, isp+maxn, 1);
+    for (int i=2; i<maxn; i++)
+        if (isp[i]) {
+            for (int j=2*i; j<maxn; j+=i)
+                isp[j] =  false;
+            primes.push_back(i);
+        }
+}
+
+void task(){
+    ll sum = 0;
+    for(auto p:primes)sum+=p;
+        cout << sum << endl;
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+#ifdef OJ
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    sieve();
+    int T = 1; //cin >> T;
+    for(int __ = 1; __ <= T; __++){
+        //cout <<"Case "<<__<<": ";
+        task();
+    }
+      
+return 0;
+}
