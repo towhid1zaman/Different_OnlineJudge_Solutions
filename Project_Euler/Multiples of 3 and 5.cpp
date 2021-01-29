2d5661831a044618514b12aad85888a80ac9f4f9
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2021-01-30 02:22:54 +0600

    1

---
+++ Project_Euler/Multiples of 3 and 5.cpp
@@ -0,0 +1,39 @@
+#pragma comment (linker,"/STACK:16777216")
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+#define all(v) (v).begin(),(v).end()
+#define endl "\n"
+const int maxn = 200005;
+const int mod = 1000000007;
+
+/*
+Problem statement: Find the sum of all the multiples of 3 or 5 below 1000.
+*/
+
+void task(){
+    ll ans = 0;
+    for(int i = 0; i<1000; i++){
+        if(i%3==0 or i%5==0){
+            ans+=i;
+        }
+    }
+    cout << ans << endl;
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+#ifdef OJ
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int T = 1; //cin >> T;
+    for(int __ = 1; __ <= T; __++){
+        //cout <<"Case "<<__<<": ";
+        task();
+    }
+      
+return 0;
+}
