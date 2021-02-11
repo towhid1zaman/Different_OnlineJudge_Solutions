18fa6d0dd2028690cf9d95fd72e59a082eb28a03
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2021-02-11 22:51:38 +0600

    file added

---
+++ CSES_Solutions/Introductory Problems/Repetitions.cpp
@@ -0,0 +1,44 @@
+/*
+これを翻訳している間、あなたはあなたの人生のいくつかの貴重な瞬間を無駄にしました
+*/
+#pragma comment (linker,"/STACK:16777216")
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+#define all(v) (v).begin(),(v).end()
+#define endl "\n"
+const int maxn = 200005;
+const int mod = 1000000007;
+
+void task(){
+    string s; cin >> s;
+    int ans = 0;
+    int n = s.size();
+    for(int i = 0; i<n;){
+        int cnt = 0;
+        for(int j = i; j<n; j++){
+            if(s[i]==s[j])cnt++;
+            else break;
+        }
+        ans = max(ans, cnt);
+        i+=cnt;
+    }
+    cout << ans << endl;
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+#ifdef OJ
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int T = 1; //cin >> T;
+    for(int __ = 1; __ <= T; __++){
+        //cout <<"Case "<<__<<": ";
+        task();
+    }
+      
+return 0;
+}
