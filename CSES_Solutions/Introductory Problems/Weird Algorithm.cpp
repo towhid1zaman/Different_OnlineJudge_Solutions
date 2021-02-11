f409a349ad0437b6f0458f16d279335c405d4be4
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2021-02-11 22:35:34 +0600

    file added

---
+++ CSES_Solutions/Introductory Problems/Weird Algorithm.cpp
@@ -0,0 +1,45 @@
+/*
+これを翻訳している間、あなたはあなたの人生のいくつかの貴重な瞬間を無駄にしました
+*/
+#pragma comment (linker,"/STACK:16777216")
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+#define all(v) (v).begin(),(v).end()
+#define endl "\n"
+const int maxn = 200005;
+const int mod = 1000000007;
+
+void task(){
+    ll n; cin >> n;
+    cout << n <<' ';
+    while(n!=1){
+        if(n%2==0){
+            n/=2;
+            cout << n<<' ';
+        }
+        else if(n%2==1){
+            n*=3;
+            n++;
+            cout << n <<' ';
+        }
+    }
+    cout << endl;
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+#ifdef OJ
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int T = 1; //cin >> T;
+    for(int __ = 1; __ <= T; __++){
+        //cout <<"Case "<<__<<": ";
+        task();
+    }
+      
+return 0;
+}
