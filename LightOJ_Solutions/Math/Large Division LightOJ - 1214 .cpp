e43365c7e6202a87d9ea00f47f0ba45cdaf4d2de
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2021-03-19 11:47:32 +0600

    file added

---
+++ LightOJ_Solutions/Math/Large Division LightOJ - 1214 .cpp
@@ -0,0 +1,44 @@
+/*
+これを翻訳している間、あなたはあなたの人生のいくつかの貴重な瞬間を無駄にしました
+*/
+#pragma comment (linker,"/STACK:16777216")
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef std::vector<int> vi;
+#define endl "\n"
+#define pb push_back
+#define rep(i,a) for(int i=0;i<a;i++)
+#define all(v) (v).begin(),(v).end()
+const int maxn = 200005;
+const int mod = 1000000007;
+
+void task(){
+    string a; cin >> a;
+    ll b; cin >> b;
+    ll d = 0, i = 0;
+    if(a[0]=='-')i = 1;
+    for(;i<a.size(); i++){
+        d = d*10 + a[i]-'0';
+        d%=b;
+    }
+    if (d) cout << "not divisible" << endl;
+    else cout << "divisible" << endl;
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+#ifdef OJ
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int T = 1; cin >> T;
+    for(int __ = 1; __ <= T; __++){
+        cout <<"Case "<<__<<": ";
+        task();
+    }
+      
+return 0;
+}
