544c986399376ad1ae83667baff055869f5b68b2
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2021-03-25 15:27:11 +0600

    file added

---
+++ UVA_Solutions/Mathematics/13071 - Double decker.cpp
@@ -0,0 +1,38 @@
+/*
+これを翻訳している間、あなたはあなたの人生のいくつかの貴重な瞬間を無駄にしました
+*/
+#pragma comment (linker,"/STACK:16777216")
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef std::vector<int> vi;
+#define endl "\n"
+#define pb push_back
+#define rep(i,a) for(int i=0;i<a;i++)
+#define all(v) (v).begin(),(v).end()
+const int maxn = 32769;
+const int mod = 1000000007;
+
+void task(){
+    int n, m; cin >> n >> m;
+    ll ans = n+m;
+    ans = (ans*(ans+1))/2 + n;
+    cout << ans + 1 << endl;
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+#ifdef OJ
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int T = 1; cin >> T;
+    for(int __ = 1; __ <= T; __++){
+        //cout <<"Case "<<__<<": ";
+        task();
+    }
+      
+return 0;
+}
