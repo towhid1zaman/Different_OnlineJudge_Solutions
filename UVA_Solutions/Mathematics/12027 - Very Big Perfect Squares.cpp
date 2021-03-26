80018190a35f77ff05012103f7aa148f3b59fe5d
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2021-03-26 22:23:02 +0600

    file added

---
+++ UVA_Solutions/Mathematics/12027 - Very Big Perfect Squares.cpp
@@ -0,0 +1,48 @@
+/*これを翻訳している間、あなたはあなたの人生のいくつかの貴重な瞬間を無駄にしました*/
+#pragma comment (linker,"/STACK:16777216")
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef std::vector<int> vi;
+#define endl "\n"
+#define pb push_back
+#define rep(i,a) for(int i=0;i<a;i++)
+#define all(v) (v).begin(),(v).end()
+const int maxn = 200005;
+const int mod = 1000000007;
+
+void task(){
+    while(true){
+        string s; cin >> s;
+        if(s=="0")break;
+        int l = s.size();
+        int n = s[0] - '0';
+        if(l%2==0){
+            n*=10;
+            n+=s[1]-'0';
+        }
+        cout << (int)sqrt(n);
+
+        int rest = (l+1)/2;
+        for(int i = 1; i<rest; i++){
+            cout <<'0';
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+    ios_base :: sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifdef _Local
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int T = 1; //cin >> T;
+    for(int __ = 1; __ <= T; __++){
+        //cout <<"Case "<<__<<": ";
+        task();
+    }
+      
+return 0;
+}
