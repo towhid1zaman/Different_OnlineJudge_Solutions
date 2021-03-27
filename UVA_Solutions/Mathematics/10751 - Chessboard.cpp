27f0ce4d759d83e79351a421ab33761237e23481
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2021-03-28 01:43:11 +0600

    file added

---
+++ UVA_Solutions/Mathematics/10751 - Chessboard.cpp
@@ -0,0 +1,37 @@
+/*これを翻訳している間、あなたはあなたの人生のいくつかの貴重な瞬間を無駄にしました*/
+#pragma comment (linker,"/STACK:16777216")
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef std::vector<int> vi;
+#define endl "\n"
+#define pb push_back
+#define rep(i,a) for(int i=0;i<a;i++)
+#define all(v) (v).begin(),(v).end()
+const int maxn = 200005;
+const int mod = 1000000007;
+
+void task(){
+    cout << setprecision(3)<<fixed;
+    long double n; cin >> n;
+    long double side = 4*(n-1), mid = (n*n) - side;
+    long double ans = side+mid*sqrt(2);
+    cout << (n>1 ? ans :0.0) << endl;
+}
+
+int main(){
+    ios_base :: sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifdef _Local
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int T = 1; cin >> T;
+    for(int __ = 1; __ <= T; __++){
+        //cout <<"Case "<<__<<": ";
+        task();
+        if(__ < T)cout << endl;
+    }
+      
+return 0;
+}
