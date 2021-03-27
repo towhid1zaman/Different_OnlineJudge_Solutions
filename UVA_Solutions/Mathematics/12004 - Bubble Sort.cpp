16a7cad48176ff41309a234c57ee14441f8272c7
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2021-03-28 01:44:10 +0600

    file added

---
+++ UVA_Solutions/Mathematics/12004 - Bubble Sort.cpp
@@ -0,0 +1,35 @@
+/*これを翻訳している間、あなたはあなたの人生のいくつかの貴重な瞬間を無駄にしました*/
+#pragma comment (linker,"/STACK:16777216")
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef std::vector<int> vi;
+#define endl "\n"
+#define pb push_back
+#define rep(i,a) for(int i=0;i<a;i++)
+#define all(v) (v).begin(),(v).end()
+const int maxn = 200005;
+const int mod = 1000000007;
+
+void task(){
+    ll n; cin >> n;
+    ll totalswaps = n*(n-1)/2;
+    if(totalswaps%2==0)cout << totalswaps/2<<endl;
+    else cout << totalswaps<<"/"<<2<<endl;
+}
+
+int main(){
+    ios_base :: sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifdef _Local
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int T = 1; cin >> T;
+    for(int __ = 1; __ <= T; __++){
+        cout <<"Case "<<__<<": ";
+        task();
+    }
+      
+return 0;
+}
