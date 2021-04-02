9cc454bd7008f8f69f228fce32d88b25e2730ad9
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2021-04-03 00:08:25 +0600

    file added

---
+++ UVA_Solutions/Mathematics/13049 - Combination Lock.cpp
@@ -0,0 +1,40 @@
+/*これを翻訳している間、あなたはあなたの人生のいくつかの貴重な瞬間を無駄にしました*/
+#pragma comment (linker,"/STACK:16777216")
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef std::vector<int> vi;
+#define endl "\n"
+#define pb push_back
+#define rep(i,a) for(int i=0;i<a;i++)
+#define all(v) (v).begin(),(v).end()
+const int maxn = 200005;
+const int mod = 1000000007;
+
+void task(){
+    int n; cin >> n;
+    string s, d; cin >> s >> d;
+    int ans = 0;
+    rep(i, n){
+        int now = s[i] - '0';
+        int desti = d[i] - '0';
+        ans+= min(abs(now - desti), 10-abs(now - desti));
+    }
+    cout << ans << endl;
+}
+
+int main(){
+    ios_base :: sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifdef _Local
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int T = 1; cin >> T;
+    for(int __ = 1; __ <= T; __++){
+        cout <<"Case "<<__<<": ";
+        task();
+    }
+      
+return 0;
+}
