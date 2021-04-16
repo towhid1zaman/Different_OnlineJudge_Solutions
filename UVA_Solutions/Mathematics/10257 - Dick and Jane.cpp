49d5e6aa2c79e0911ae66674a1db738d0f09b6ca
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2021-04-16 12:41:35 +0600

    file added

---
+++ UVA_Solutions/Mathematics/10257 - Dick and Jane.cpp
@@ -0,0 +1,42 @@
+/*これを翻訳している間、あなたはあなたの人生のいくつかの貴重な瞬間を無駄にしました*/
+#pragma comment (linker,"/STACK:16777216")
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef std::vector<int> vi;
+typedef std::vector<ll> vll;
+#define endl "\n"
+#define pb push_back
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define rep(i,a) for(int i=0;i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define all(v) (v).begin(),(v).end()
+const int maxn = 200005;
+const int mod = 1000000007;
+
+void task(){
+    ll s, p, y, j, d = 12ll; 
+    while(cin >> s >> p >> y >> j){
+        ll yertle = (d+j-y-p)/3;
+        ll puff  = (d+j-s-yertle)/2;
+        ll spot  = (d+j-puff-yertle);
+        cout << spot <<' '<< puff <<' '<< yertle<<endl;
+    }
+
+}
+
+int main(){
+    ios_base :: sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifdef _Local
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int T = 1; //cin >> T;
+    for(int __ = 1; __ <= T; __++){
+        //cout <<"Case "<<__<<": ";
+        task();
+    }
+      
+return 0;
+}
