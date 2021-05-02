ac0e1675cee7ebd6369aa1508baafb735001050d
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2021-05-02 12:43:14 +0600

    file added

---
+++ UVA_Solutions/Mathematics/11970 - Lucky Numbers.cpp
@@ -0,0 +1,48 @@
+/*これを翻訳している間、あなたはあなたの人生のいくつかの貴重な瞬間を無駄にしました*/
+#pragma comment (linker,"/STACK:16777216")
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef std::vector<int> vi;
+typedef std::vector<ll> vll;
+#define endl "\n"
+#define pb push_back
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define rep(i,a) for(int i=0;i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define all(v) (v).begin(),(v).end()
+
+const double pi = acos(-1.0);
+const int maxn = 200005;
+const int mod = 1000000007;
+
+void task(){
+    ll n; cin >> n;
+    vll X;
+    X.clear();
+    for(ll i = sqrt(n) - 1; i>=1; i--){
+      ll x = n-i*i;
+      if(x%i==0)X.push_back(x);
+    }
+    for(int i = 0; i<X.size(); i++){
+      cout << X[i];
+      if(i<X.size()-1)cout << ' ';
+    }
+    cout << endl;
+}
+
+int main(){
+    ios_base :: sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifdef _Local
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int T = 1; cin >> T;
+    for(int __ = 1; __ <= T; __++){
+        cout <<"Case "<<__<<": ";
+        task();
+    }
+      
+return 0;
+}
