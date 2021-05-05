61790e637df1a868895da94caac10a6b2752d4bd
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2021-05-05 12:45:33 +0600

    file added

---
+++ UVA_Solutions/Mathematics/686 - Goldbach's Conjecture (II).cpp
@@ -0,0 +1,76 @@
+/*これを翻訳している間、あなたはあなたの人生のいくつかの貴重な瞬間を無駄にしました*/
+#pragma comment (linker,"/STACK:16777216")
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef std::vector<int> vi;
+typedef std::vector<ll> vll;
+#define endl "\n"
+#define pb push_back
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define rep(i,a) for(int i=0;i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define all(v) (v).begin(),(v).end()
+
+const double pi = acos(-1.0);
+const int maxn = 32770;
+const int mod = 1000000007;
+template<typename F,typename S>ostream&operator<<(ostream&os,const pair<F,S>&p){return os<<"("<<p.first<<", "<<p.second<<")";}
+template<typename T>ostream&operator<<(ostream&os,const vector<T>&v){os<<"{";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<", ";os<<*it;}return os<<"}";}
+template<typename T>ostream&operator<<(ostream&os,const set<T>&v){os<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<",";os<<*it;}return os<<"]";}
+template<typename T>ostream&operator<<(ostream&os,const multiset<T>&v) {os<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<", ";os<<*it;}return os<<"]";}
+template<typename F,typename S>ostream&operator<<(ostream&os,const map<F,S>&v){os<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<", ";os<<it->first<<" = "<<it->second;}return os<<"]";}
+#define dbug(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+void faltu(){cerr << endl;}
+template<typename T>void faltu(T a[],int n){for(int i=0;i<n;++i)cerr<<a[i]<<' ';cerr<<endl;}
+template<typename T,typename...hello>void faltu(T arg,const hello&...rest){cerr<<arg<<' ';faltu(rest...);}
+
+
+bool isPrime(int n){
+        if(n==2){
+            return true;
+        }
+        if(n<2 || n%2==0){
+            return false;
+        }
+        for(int i = 3; i*i<=n;i+=2){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+}
+void task(){
+    int n, C;
+    while(cin >> n){
+        if(!n)break;
+        set< pair<int, int> >s;
+        s.clear();
+        int mx, mn;
+        for(int i = 2; i<=n; i++){
+            mn = i, mx = n-i;
+            if(isPrime(mx) and isPrime(mn)){
+                if(mn > mx)swap(mn, mx);
+                if(mx+mn==n)s.emplace(mn, mx);
+            }
+        }
+        //dbug(s);
+        cout << s.size() << endl;
+    }
+}
+
+int main(){
+    ios_base :: sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifdef _Local
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int T = 1; //cin >> T;
+    for(int __ = 1; __ <= T; __++){
+        //cout <<"Case "<<__<<": ";
+        task();
+    }
+      
+return 0;
+}
