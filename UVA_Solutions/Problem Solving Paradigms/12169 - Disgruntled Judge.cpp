69d33784df850cca5e9246d3d09918a36a1d2d34
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2021-05-24 12:44:52 +0600

    file added

---
+++ UVA_Solutions/Problem Solving Paradigms/12169 - Disgruntled Judge.cpp
@@ -0,0 +1,57 @@
+/*これを翻訳している間、あなたはあなたの人生のいくつかの貴重な瞬間を無駄にしました*/
+#pragma comment (linker,"/STACK:16777216")
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef std::vector<int> vi;
+typedef std::vector<ll> vll;
+#define endl "\n"
+#define pb push_back
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define rep(i,a) for(int i=0;i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define all(v) (v).begin(),(v).end()
+
+const double pi = acos(-1.0);
+const int maxn = 10005;
+const int mod = 10001;
+int x[maxn];
+void task(){
+    int T; cin >> T;
+    for(int i = 1; i<= T+T; i+=2){
+        cin >> x[i];
+    }
+    for(int a = 0; a<=maxn; a++){
+        for(int b = 0; b<=maxn; b++){
+            bool ok = 1;
+            for(int i = 2; i<=T+T; i+=2){
+                x[i] = (a*x[i-1]+b)%mod;
+                if(i < T+T and x[i+1] != (a*x[i] + b)%mod){
+                    ok = 0;
+                    break;
+                }
+            }
+            if(ok)goto ok;
+        }
+    }
+    ok:;
+    for(int i = 2; i<=T+T; i+=2){
+        cout << x[i] << endl;
+    }
+}
+
+int main(){
+    ios_base :: sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifdef _Local
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int T = 1; //cin >> T;
+    for(int __ = 1; __ <= T; __++){
+        //cout <<"Case "<<__<<": ";
+        task();
+    }
+      
+return 0;
+}
