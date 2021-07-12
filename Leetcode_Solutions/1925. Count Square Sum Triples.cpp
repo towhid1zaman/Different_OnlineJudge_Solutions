8f6e019cac55b6aab7e37c3bba517993b88c9f3c
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2021-07-12 11:05:44 +0600

    math

---
+++ Leetcode_Solutions/1925. Count Square Sum Triples.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countTriples(int n) {
+        int ans = 0;
+        for(int a = 1; a<=n; a++){
+            for(int b = 1; b<=n; b++){
+                for(int c = 1; c<=n; c++){
+                    if((a*a)+(b*b) == (c*c) and (a!=b and b!=c))ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
