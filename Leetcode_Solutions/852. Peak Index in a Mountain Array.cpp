ba6f9bcecaa91364d0330c8dc1c730191f650e35
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2021-10-02 23:26:57 +0600

    loop

---
+++ Leetcode_Solutions/852. Peak Index in a Mountain Array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+int peakIndexInMountainArray(vector<int>& arr) {
+    int ans = 0;
+    for(int i = 1; i<arr.size(); i++){
+        if(arr[i] < arr[i-1]){
+            ans = i-1;
+            break;
+        }
+    }
+    return ans;
+}
+};
