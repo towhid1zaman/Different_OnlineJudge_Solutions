edcd2e4d38ec477142cb381b88af4d87626d7306
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2022-01-26 14:11:32 +0600

    string

---
+++ Leetcode_Solutions/1446. Consecutive Characters.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxPower(string s) {
+        int ans = 1;
+        int cnt = 1;
+        for(int i = 1; i<=s.size(); i++){
+            if(s[i]!=s[i-1]){
+                ans = max(ans, cnt);
+                cnt = 1;
+            }
+            else cnt++;
+        }
+        return ans;
+    }
+};
