c11ded75e9e76e966a554f7ec79019a8c7793b16
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2022-01-26 13:34:33 +0600

    array

--- Leetcode_Solutions/41. First Missing Positive.cpp
+++ Leetcode_Solutions/41. First Missing Positive.cpp
@@ -1,12 +1,28 @@
 class Solution {
 public:
-    int firstMissingPositive(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int n = (int)nums.size();
-        int mex = 1;
-        for(int i = 0; i<n; i++){
-            if(nums[i] == mex)mex++;
+    int firstMissingPositive(vector<int>& v) {
+    int n = v.size();
+    int one = 0;
+    for(int i = 0; i<n; i++){
+        if(v[i] == 1)one = 1;
+        if(v[i]<=0 or v[i]>n){
+            v[i] = 1;
         }
-        return mex;
+    }
+    if(!one){
+        return 1;
+    }
+    for(int i = 0;i<n; i++){
+        int ind = abs(v[i]) - 1;
+        if(v[ind]>0)v[ind] = -v[ind];
+    }
+    int ans = n+1;
+    for(int i = 0; i<n;i++){
+        if(v[i] > 0){
+            ans = i+1;
+            break;
+        }
+    }
+    return ans;
     }
 };
