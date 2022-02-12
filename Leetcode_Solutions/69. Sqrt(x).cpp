66f6f8458f5a1c8caf6ea8d83418b2c74f7561fa
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2022-02-12 18:26:51 +0600

    BS

---
+++ Leetcode_Solutions/69. Sqrt(x).cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int low = 1, hi = x;
+        int ans;
+        while(low <= hi){
+            int mid = low + (hi - low) / 2;
+            if(mid <= x/mid){
+                ans = mid;
+                low = mid+1;
+            }
+            else hi = mid - 1;
+        }
+        return ans;
+    }
+};
