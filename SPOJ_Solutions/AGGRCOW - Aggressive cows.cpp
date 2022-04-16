f84fff83cf9556930f14317757b7c1e6992aef8d
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2022-04-16 16:10:35 +0600

    Binary search

--- SPOJ_Solutions/AGGRCOW - Aggressive cows.cpp
+++ SPOJ_Solutions/AGGRCOW - Aggressive cows.cpp
@@ -2,41 +2,48 @@
 using namespace std;
 typedef long long ll;
 
- 
-bool check(std::vector<int> a, int c, int minD){
+int checkgap(std::vector<int> v, int gap, int c){
+    int last = v[0];
     int cnt = 1;
-    int last = a[0];
-    for(int i = 1; i<a.size(); i++){
-        if(a[i] - last >= minD){
-            last = a[i];
+    for(int i = 1; i<v.size(); i++){
+        if(v[i] - last >= gap){
+            last = v[i];
             cnt++;
+            if(cnt == c)return 1;
         }
-        if(cnt == c)return true;
     }
-    return false;
+    return 0;
+}
+
+void task(){
+   int n, c; cin >> n >> c;
+   std::vector<int> a(n);
+   for(int i = 0; i<n; i++){
+    cin >> a[i];
+   }
+   int ans = 0;
+   sort(a.begin(), a.end());
+   int low = 1, high = a[0] + a[n-1] + 1;
+   while(low <= high){
+    int gap = (low + high)/2;
+    if(checkgap(a, gap, c)){
+        ans = max(ans, gap);
+        low = gap+1;
+    }
+    else high = gap - 1;
+   }
+   cout << ans << endl;
 }
  
 int main(){
+    //ctrl+alt+b
+    ios_base :: sync_with_stdio(0); cin.tie(0); cout.tie(0);
     
-    int t; cin >> t;
-    while(t--){
-        int n, c; cin >> n >> c;
-        std::vector<int> a(n);
-        for(int i = 0; i<n; i++){
-            cin >> a[i];
-        }
-        sort(a.begin(), a.end());
-        ll r = a[0] + a[n-1] + 1;
-        ll l = 0;
-        while(r-l > 0){
-            ll mid = (r+l+1)/2;
-            if(check(a, c, mid)){
-                l = mid;
-            }
-            else r = mid - 1;
-        }
-        cout << l << endl;
+    int T = 1;cin >> T;
+    for(int __ = 1; __ <= T; __++){
+        //cout <<"Case "<<__<<": "<<endl;
+        task();
     }
+      
 return 0;
 }
- 
