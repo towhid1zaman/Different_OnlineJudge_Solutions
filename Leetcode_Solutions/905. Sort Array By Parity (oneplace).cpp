c2ad0681b039a57dc549c717c311e95d0255c28d
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2022-08-18 18:47:33 +0600

    two pointers

---
+++ Leetcode_Solutions/905. Sort Array By Parity (oneplace).cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int n = (int)nums.size();
+        int startPos = 0, curPos = 0;
+        while(curPos < n){
+            if(nums[curPos]%2==0){
+                swap(nums[startPos++], nums[curPos++]);
+            }
+            else curPos++;
+        }
+        return nums;
+    }
+};
